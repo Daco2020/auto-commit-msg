075aee7a20f946df63f37aa4c81c91caabe97102
Daco2020 <dacokim32@gmail.com>
2023-08-15 18:32:02 +0900

    chore: Modified to not operate when there's a commit source

--- config.py
+++ config.py
@@ -21,10 +21,10 @@ commit_msg_file=$1
 commit_source=$2
 commit_msg=$(cat $commit_msg_file)
 
-if [ "$commit_source" == "message" ]; then
-    if [[ $commit_msg == *"'#'"* ]]; then
-        echo $(cat .git/temp_commit_msg) > $commit_msg_file
-        rm .git/temp_commit_msg
-    fi
+if [ "$commit_source" == "message" ] || [ "$commit_source" == "template" ] || [ "$commit_source" == "merge" ] || [ "$commit_source" == "squash" ] || [ "$commit_source" == "commit" ]; then
+    exit 0
 fi
+
+echo $(cat .git/temp_commit_msg) > $commit_msg_file
+rm .git/temp_commit_msg
 """
